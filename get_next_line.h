3dcb735a22504f5ca0772ee9588e0af1813fe4c6
linnitel <61269859+linnitel@users.noreply.github.com>
2020-11-06 16:28:34 +0700

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(const char *s);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s1);
+char		*ft_strjoin(char const *s1, char const *s2);
+
+#endif
